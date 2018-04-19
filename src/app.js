3709caa38093e54b74df09a6cf0598141adb77b5
David Wolgemuth <davidjwolgemuth@gmail.com>
2018-04-18 23:00:39 -0500

    handle mood numbers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 // Alexa handlers
 const handlers = {
   'GeneralMoodIntent': function () {
-    this.emit(':ask', 'Thank you! What words describe your mood?');
+    const mood = slotValue(this.event, 'OverallMood');
+    if (mood < 0 || mood > 10) {
+      return this.emit(
+        ':ask',
+        'Sorry, please say a number between 1 and 10.  What is your mood?'
+      );
+    } 
+    if (mood < 5) {
+      return this.emit(
+        ':ask',
+        'Sorry you feel that way. What word describes your mood?'
+      );
+    } else {
+      return this.emit(
+        ':ask',
+        'Thank you! What words describe your mood?'
+      );
+    }
   },
   'MoodWordIntent': function () {
     this.emit(':ask', 'Alright.  Do you have other words to describe your mood?');
@@ -10,5 +27,7 @@ const handlers = {
     this.emit(':ask', 'What is your overall mood from one to ten?');
   }
 };
+
+const slotValue = (event, slotName) => event.request.intent.slots[slotName].value;
 
 module.exports = { handlers };
