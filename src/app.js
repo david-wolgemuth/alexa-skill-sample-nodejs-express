c5573a8df4509b793700cdb3484c6357d272ab69
David Wolgemuth <davidjwolgemuth@gmail.com>
2018-04-18 23:12:41 -0500

    reprompts

--- src/app.js
+++ src/app.js
@@ -5,26 +5,29 @@ const handlers = {
     if (mood < 0 || mood > 10) {
       return this.emit(
         ':ask',
-        'Sorry, please say a number between 1 and 10.  What is your mood?'
+        'Sorry, please say a number between 1 and 10.  What is your mood?',
+        'Again?'
       );
     } 
     if (mood < 5) {
       return this.emit(
         ':ask',
-        'Sorry you feel that way. What word describes your mood?'
+        'Sorry you feel that way. What word describes your mood?',
+        'Again?'
       );
     } else {
       return this.emit(
         ':ask',
-        'Thank you! What words describe your mood?'
+        'Thank you! What words describe your mood?',
+        'Again?'
       );
     }
   },
   'MoodWordIntent': function () {
-    this.emit(':ask', 'Alright.  Do you have other words to describe your mood?');
+    this.emit(':ask', 'Alright.  Do you have other words to describe your mood?', 'What?');
   },
   'Unhandled': function () {
-    this.emit(':ask', 'What is your overall mood from one to ten?');
+    this.emit(':ask', 'What is your overall mood from one to ten?', 'What?');
   }
 };
 
