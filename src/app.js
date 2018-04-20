3894cb772185a538194912c09d2d0651d2f5fc75
David Wolgemuth <davidjwolgemuth@gmail.com>
2018-04-19 21:13:32 -0500

    remove \'

--- src/app.js
+++ src/app.js
@@ -7,21 +7,18 @@ const handlers = {
     if (mood < 0 || mood > 10) {
       return this.emit(
         ':ask',
-        'Sorry, please say a number between 1 and 10.  What is your mood?',
-        'Again?'
+        'Sorry, please say a number between 1 and 10.  What is your mood?'
       );
     } 
     if (mood < 5) {
       return this.emit(
         ':ask',
-        'Sorry you feel that way. What word describes your mood?',
-        'Again?'
+        'Sorry you feel that way. What word describes your mood?'
       );
     } else {
       return this.emit(
         ':ask',
-        'Thank you! What words describe your mood?',
-        'Again?'
+        'Thank you! What words describe your mood?'
       );
     }
   },
@@ -37,10 +34,10 @@ const handlers = {
     this.emit(':ask', 'Alright.  Do you have other words to describe your mood?', 'What?');
   },
   'AMAZON.HelpIntent': function () {
-    this.emit(':ask', 'Say something like, "My overall mood is 8 out of 10", or "I am feeling ninety percent happy".');
+    this.emit(':ask', 'Say something like, "My overall mood is 8 out of 10", or "I am feeling ninety percent happy". How are you feeling?');
   },
   'Unhandled': function () {
-    this.emit(':ask', 'Sorry, I\'m not sure how to handle that one.');
+    this.emit(':ask', 'Sorry, I am not sure how to handle that one.');
   }
 };
 
