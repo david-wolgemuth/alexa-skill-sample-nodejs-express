d9491f2ba89e1e61457a2e26914e2a1a17d56f3b
David Wolgemuth <davidjwolgemuth@gmail.com>
2018-04-19 22:35:49 -0500

    add middleware functions

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ module.exports = convertedHandlers = () => {
 const convertHandler = (handler) => function () {
   const handle = this;
   handle.slots = getSlots(this);
+  handle.ask = ask;
+  handle.requestMissingInfo = requestMissingInfo;
+  handle.user = fetchUser(this.event);
+  handle.unhandled = unhandled;
   handler(handle);
 };
 
@@ -23,4 +27,25 @@ const getSlots = handle => {
     slots[slotName] = rawSlots[slotName].value;
   }
   return slots;
+};
+
+const ask = function (question, prompt) {
+  this.emit(':ask', question, prompt);
+};
+
+const fetchUser = event => {
+  const userId = event.context.System.user.userId;
+  return {
+    userId,
+    addMoodWord: function () { console.log('ADD MOOD WORD', arguments); },
+    addOverallMood: function () { console.log('ADD OVERALL MOOD', arguments); }
+  };
+};
+
+const requestMissingInfo = function() {
+  this.emit(':delegate');
+};
+
+const unhandled = function () {
+  this.emit('Unhandled');
 };
