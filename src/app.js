36f32910c1b8d533e074c31682969127352da8c9
David Wolgemuth <davidjwolgemuth@gmail.com>
2018-04-19 21:08:16 -0500

    undid last changes

--- src/app.js
+++ src/app.js
@@ -5,15 +5,24 @@ const handlers = {
   'GeneralMoodIntent': function () {
     const mood = slotValue(this.event, 'OverallMood');
     if (mood < 0 || mood > 10) {
-      this.emit(':tell', 'Sorry, please say a number between 1 and 10.')
-      return this.emit(':ask', 'What is your mood?');
+      return this.emit(
+        ':ask',
+        'Sorry, please say a number between 1 and 10.  What is your mood?',
+        'Again?'
+      );
     } 
     if (mood < 5) {
-      this.emit(':tell', 'Sorry you feel that way.');
-      return this.emit(':ask', 'What word describes your mood?');
+      return this.emit(
+        ':ask',
+        'Sorry you feel that way. What word describes your mood?',
+        'Again?'
+      );
     } else {
-      this.emit(':tell', 'Thank you!');
-      return this.emit(':ask', 'What words describe your mood?');
+      return this.emit(
+        ':ask',
+        'Thank you! What words describe your mood?',
+        'Again?'
+      );
     }
   },
   'MoodWordIntent': function () {
@@ -25,15 +34,13 @@ const handlers = {
     if (!moodWord) {
       return this.emit(':tell', 'Hm. that was weird.');
     }
-    this.emit(':ask', 'Alright.  Do you have other words to describe your mood?');
+    this.emit(':ask', 'Alright.  Do you have other words to describe your mood?', 'What?');
   },
   'AMAZON.HelpIntent': function () {
-    this.emit(':tell', 'Say something like, "My overall mood is 8 out of 10", or "I am feeling ninety percent happy".');
-    this.emit(':ask', 'How do you feel?');
+    this.emit(':ask', 'Say something like, "My overall mood is 8 out of 10", or "I am feeling ninety percent happy".');
   },
   'Unhandled': function () {
-    this.emit(':tell', 'Sorry, I\'m not sure how to handle that one.');
-    this.emit('AMAZON.HelpIntent');
+    this.emit(':ask', 'Sorry, I\'m not sure how to handle that one.');
   }
 };
 
